5ad3d54d19a1fcc7699e845d0c9846df1a3173fd
DANIEL ESTEBAN SALAZAR VAHOS <desalazar@correo.iue.edu.co>
2025-10-19 22:55:17 -0500

    Add coverage generation for SonarCloud

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,8 +1,6 @@
 export default {
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/test/setup/jest.teardown.js'],
-  transform: {
-    '^.+\\.js$': 'babel-jest'
-  }
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text-summary"]
 };
-
